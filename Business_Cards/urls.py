31179c4301b3cc2d79bf35bb29dba1a3c20b934c
Umair-34 <Umairmuhammad433@gmail.com>
2021-01-31 12:47:00 +0500

    business-cards-added

--- Business_Cards/urls.py
+++ Business_Cards/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
-from .views import Edge_painted_Detail, BC_Detail
+from .views import Edge_painted_Detail, BC_Detail, Foil_business_card, Raised_spot_uv, Pantone_business_cards
 
 app_name='Business_Cards'
 
 urlpatterns = [
     path('edge-painted-detail', Edge_painted_Detail, name='edge-painted-detail'),
     path('business-card-detail', BC_Detail, name='business-card-detail'),
+    path('foil-business-card-detail', Foil_business_card , name='foil-business-card-detail'),
+    path('raised-spot-uv-business-card-detail', Raised_spot_uv , name='raised-spot-uv-business-card-detail'),
+    path('pantone-business-cards-detail', Pantone_business_cards, name='pantone-business-cards-detail'),
 
 ]
