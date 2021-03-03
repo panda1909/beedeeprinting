40cf800d211f921087dc13dc1783eee0bb11bfc0
panda1909 <umerfarooqb97@gmail.com>
2021-03-03 21:00:23 +0500

    static root

--- beedeeprinting/settings.py
+++ beedeeprinting/settings.py
@@ -145,7 +145,7 @@ EMAIL_HOST_PASSWORD = "mlrliyridouecwze"
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATIC_ROOT = os.path.join(BASE_DIR, "stactic")
 
 # django_heroku.settings(locals())
